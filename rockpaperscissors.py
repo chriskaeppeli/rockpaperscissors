fd38922c4af780fb0394f3913012fab9cc29175e
chriskaeppeli <16383159+chriskaeppeli@users.noreply.github.com>
2021-11-25 23:20:37 +0100

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,1 +1,45 @@
 # In this document I will write a python code to play rock,paper,scissors
+
+import sys
+import random
+
+
+#Getting an the input of users
+
+def yourChoice():
+    print("Press 'R' for Rock.")
+    print("Press 'P' for Paper.")
+    print("Press 'S' for Scissors.")
+    print("Press 'Q' to Quit.")
+    #print("Press 'X' for Score reset.")   <-- for a later implementation of scorepoints if the simple program works
+    
+    user_choice = input("What do you wanna choose?").lower()
+
+    if user_choice == "r":
+        return "Rock"
+    elif user_choice == "p":
+        return "Paper"
+    elif user_choice == "s":
+        return "Scissors"
+    elif user_choice == "q":
+        sys.exit(0)
+    else:
+        yourChoice()
+
+
+#Random Choice of the python programm
+
+def pythonRandom():
+    rps = ["Rock", "Paper", "Scissors"]
+    randChoice = random.randint(0,2)
+    return rps[randChoice]
+
+#Comparison of the users input and the random choice of the python program
+
+def
+
+
+
+
+
+
